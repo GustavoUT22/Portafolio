ba6918fe655440aa17cf425134a7725163f2626d
Gustavout22 <gustavougartetorres@gmail.com>
2023-11-05 01:05:29 -0500

    chore: structure of work

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,91 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <nav>
+          <img></img>
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Portafolio</li>
+            <li>Blog</li>
+            <li>Contact us</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section>
+          <div>foto de perfil</div>
+          <div>
+            <h4>Hi, My name Gustavo</h4>
+            <span>Full Stack Developer</span>
+          </div>
+        </section>
+        <section>
+          <h2>Who am I</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+            quos culpa placeat quae. Pariatur quisquam facilis consectetur illum
+            voluptas, aut quas consequuntur amet. Cumque molestias, saepe
+            repudiandae accusantium quo unde.
+          </p>
+        </section>
+        <section>
+          <h2>Skills</h2>
+          <ul>
+            <li>Python</li>
+            <li>TypeScript</li>
+            <li>Ruby On Rails</li>
+            <li>NodeJS</li>
+            <li>ReactJs</li>
+            <li>PostgresQl</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Proyectos</h2>
+          <div>
+            <div>
+              <span>Nombre del proyecto</span>
+              <div>Imagen del proyecto</div>
+              <a href="#">go repo</a>
+              <a href="#">open project</a>
+            </div>
+            <div>
+              <span>Nombre del proyecto</span>
+              <div>Imagen del proyecto</div>
+              <a href="#">go repo</a>
+              <a href="#">open project</a>
+            </div>
+            <div>
+              <span>Nombre del proyecto</span>
+              <div>Imagen del proyecto</div>
+              <a href="#">go repo</a>
+              <a href="#">open project</a>
+            </div>
+            <div>
+              <span>Nombre del proyecto</span>
+              <div>Imagen del proyecto</div>
+              <a href="#">go repo</a>
+              <a href="#">open project</a>
+            </div>
+          </div>
+        </section>
+        <section>
+          <h2> Contact me</h2>
+          <form>
+            <label>Name</label>
+            <input type="text"/>
+            <label>Email</label>
+            <input type="text"/>
+            <label>Message</label>
+            <input type="text"/>
+            <button>Send</button>
+          </form>
+        </section>
+      </main>
+      <footer></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
