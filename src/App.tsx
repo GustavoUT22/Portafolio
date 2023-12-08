5e8272005faf88316a26e0c285ca02f10712b413
Gustavout22 <gustavougartetorres@gmail.com>
2023-12-07 23:46:52 -0500

    modify structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,25 @@
 import ProfilePresentacion from "./pages/ProfilePresentacion/profile-presentacion";
-import Navbar from "./components/NavBar/Navbar";
 import Container from "./utils/container/Container";
+import About from "./pages/AboutMe/About";
+import Projects from "./pages/Projects/Projects";
+import Skills from "./pages/Skills/Skills";
+import ContactMe from "./pages/ContactMe/ContactMe";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <Container size="xl">
-      <header style={{width:"100%"}}>
-        <Navbar/>
-      </header>
-      <main>
+    <div>
+      <Header/>
+      <Container size="xl">
         <ProfilePresentacion/>
-        <section>
-          <h2>About me</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            quos culpa placeat quae. Pariatur quisquam facilis consectetur illum
-            voluptas, aut quas consequuntur amet. Cumque molestias, saepe
-            repudiandae accusantium quo unde.
-          </p>
-        </section>
-        <section>
-          <h2>Skills</h2>
-          <ul>
-            <li>Python</li>
-            <li>TypeScript</li>
-            <li>Ruby On Rails</li>
-            <li>NodeJS</li>
-            <li>ReactJs</li>
-            <li>PostgresQl</li>
-          </ul>
-        </section>
-        <section>
-          <h2>Proyectos</h2>
-          <div>
-            <div>
-              <span>Nombre del proyecto</span>
-              <div>Imagen del proyecto</div>
-              <a href="#">go repo</a>
-              <a href="#">open project</a>
-            </div>
-            <div>
-              <span>Nombre del proyecto</span>
-              <div>Imagen del proyecto</div>
-              <a href="#">go repo</a>
-              <a href="#">open project</a>
-            </div>
-            <div>
-              <span>Nombre del proyecto</span>
-              <div>Imagen del proyecto</div>
-              <a href="#">go repo</a>
-              <a href="#">open project</a>
-            </div>
-            <div>
-              <span>Nombre del proyecto</span>
-              <div>Imagen del proyecto</div>
-              <a href="#">go repo</a>
-              <a href="#">open project</a>
-            </div>
-          </div>
-        </section>
-        <section>
-          <h2> Contact me</h2>
-          <form>
-            <label>Name</label>
-            <input type="text"/>
-            <label>Email</label>
-            <input type="text"/>
-            <label>Message</label>
-            <input type="text"/>
-            <button>Send</button>
-          </form>
-        </section>
-      </main>
-      <footer></footer>
-    </Container>
+        <About/>
+        <Projects/>
+        <Skills/>
+        <ContactMe/>
+      </Container>
+      <Footer/>
+    </div>
   );
 }
 
--- src/pages/ProfilePresentacion/profile-presentacion.tsx
+++ src/pages/ProfilePresentacion/profile-presentacion.tsx
@@ -1,33 +1,39 @@
 import React from "react";
 import styled from "styled-components";
-import imageprofile from "../../assets/images/foto-perfil-1.jpg"
+import imageprofile from "../../assets/images/foto-perfil-1.jpg";
 
-const SpaceBetween = styled.div<React.HTMLAttributes<HTMLDivElement>>`
+const SpaceBetween = styled.div`
   display: flex;
   flex-wrap: wrap;
   flex-direction: row-reverse;
   justify-content: space-around;
-`
-const FotoPerfil = styled.img<React.HtmlHTMLAttributes<HTMLImageElement>>`
+`;
+const FotoPerfil = styled.img`
   border-radius: 100px;
   width: 200px;
   height: auto;
-`
+`;
 
+const ContainerPresentacion = styled.section`
+  height: 30vh;
+  background-color: beige;
+  width: 100%;
+`;
 
-const ProfilePresentacion: React.FC = (): React.ReactElement=> {
+const ProfilePresentacion: React.FC = (): React.ReactElement => {
   return (
-    <SpaceBetween>
-      <div>
-        <FotoPerfil src={imageprofile}/>
-      </div>
-      <div>
-        <p>Hello, I'm</p>
-        <h2>Gustavo Ugarte</h2>
-        <h3>Software Developer</h3>
-      </div>
-  
-    </SpaceBetween>
+    <ContainerPresentacion>
+      <SpaceBetween>
+        <div>
+          <FotoPerfil src={imageprofile} />
+        </div>
+        <div>
+          <p>Hello, I'm</p>
+          <h2>Gustavo Ugarte</h2>
+          <h3>Software Developer</h3>
+        </div>
+      </SpaceBetween>
+    </ContainerPresentacion>
   );
 };
 
